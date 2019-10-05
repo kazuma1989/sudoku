de75b89ba38866415e4b6cda68a03e5a4c4746aa
kazuma1989 <funifuni.1204@gmail.com>
2019-10-05 17:28:20 +0900

    fix: Not to crash when using Button
    React >= 16.9 does not seem to be compatible with React Native Web...

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.box}>
       <Text style={styles.text}>Hello, world!</Text>
+      <Button title={`${count}`} onPress={() => setCount(i => i + 1)} />
     </View>
   )
 }
