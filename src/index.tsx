04647c54219f5a64ed27170aae1a1d75cdc208f7
kazuma1989 <funifuni.1204@gmail.com>
2019-10-05 21:00:38 +0900

    feat: Place numbers

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,84 @@
 import React, { useState } from 'react'
-import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native'
+import { AppRegistry, StyleSheet, Text, View, ViewStyle } from 'react-native'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [areas] = useState([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  ])
 
   return (
-    <View style={styles.box}>
-      <Text style={styles.text}>Hello, world!</Text>
-      <Button title={`${count}`} onPress={() => setCount(i => i + 1)} />
+    <View style={styles.container}>
+      <View style={styles.areaContainer}>
+        {areas.map((area, i) => (
+          <View key={i} style={styles.area}>
+            {area.map((cell, j) => (
+              <View key={`${i}-${j}`} style={styles.cell}>
+                <Text style={styles.cellText}>{cell}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
     </View>
   )
 }
 
+const border: ViewStyle = {
+  borderStyle: 'solid',
+  borderColor: 'silver',
+  borderWidth: 1,
+}
+
+const cell = {
+  size: 70,
+  fontSize: 40,
+}
+
+const areaSize = cell.size * 3 + border.borderWidth * 2
+const areaContainerSize = areaSize * 3 + border.borderWidth * 2 + 2
+
 const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: 'bold' },
+  container: {
+    padding: 10,
+  },
+
+  areaContainer: {
+    ...border,
+    borderWidth: 2,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: areaContainerSize,
+    height: areaContainerSize,
+  },
+
+  area: {
+    ...border,
+    width: areaSize,
+    height: areaSize,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  cell: {
+    ...border,
+    width: cell.size,
+    height: cell.size,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  cellText: {
+    fontWeight: 'bold',
+    fontSize: cell.fontSize,
+  },
 })
 
 AppRegistry.registerComponent('App', () => App)
